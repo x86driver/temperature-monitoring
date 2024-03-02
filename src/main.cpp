f705230da790a9de4825e3bed4a8fa1164ffeefb
Shane Lin <253692+doremi@users.noreply.github.com>
2024-03-02 16:14:10 +0800

    Can send the data from Arduino Nano 33 BLE Sense Rev2

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,74 @@
 #include <Arduino.h>
+#include <RadioLib.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#if defined(ESP32)
+SX1268 radio = new Module(5, 25, 16, -1);
+#elif defined(ARDUINO_ARDUINO_NANO33BLE)
+// Arduino Nano 33 BLE Sense Rev2
+SX1278 radio = new Module(5, 7, 6, RADIOLIB_NC);
+#else
+#error "Please select a board!"
+#endif
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  while (!Serial);
+
+  while (true) {
+    // initialize SX1278 with default settings
+    Serial.print(F("[SX1278] Initializing ... "));
+    int state = radio.begin();
+    if (state == RADIOLIB_ERR_NONE) {
+      Serial.println(F("success!"));
+      break;
+    } else {
+      Serial.print(F("failed, code "));
+      Serial.println(state);
+      delay(1000);
+    }
+  }
 }
+
+int count = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  Serial.print(F("[SX1278] Transmitting packet ... "));
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // you can transmit C-string or Arduino string up to
+  // 255 characters long
+  String str = "Hello World! #" + String(count++);
+  int state = radio.transmit(str);
+
+  // you can also transmit byte array up to 256 bytes long
+  /*
+    byte byteArr[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+    int state = radio.transmit(byteArr, 8);
+  */
+
+  if (state == RADIOLIB_ERR_NONE) {
+    // the packet was successfully transmitted
+    Serial.println(F(" success!"));
+
+    // print measured data rate
+    Serial.print(F("[SX1278] Datarate:\t"));
+    Serial.print(radio.getDataRate());
+    Serial.println(F(" bps"));
+
+  } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
+    // the supplied packet was longer than 256 bytes
+    Serial.println(F("too long!"));
+
+  } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
+    // timeout occurred while transmitting packet
+    Serial.println(F("timeout!"));
+
+  } else {
+    // some other error occurred
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+
+  }
+
+  // wait for a second before transmitting again
+  delay(1000);
 }
